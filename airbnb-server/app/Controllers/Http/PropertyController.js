5218af629702cb6d5d2854bc18cbef2acc45c37c
José Pedro Simão <josepedrosimao@gmail.com>
2020-06-29 23:27:54 -0300

    start adonis and routes

---
+++ airbnb-server/app/Controllers/Http/PropertyController.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+class PropertyController {
+  /**
+   * Show a list of all properties.
+   * GET properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request }) {
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .nearBy(latitude, longitude, 10) //acos not working on sqlite3
+      .fetch()
+
+    return properties
+  }
+
+
+  /**
+   * Create/save a new property.
+   * POST properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
+  }
+  /**
+   * Display a single property.
+   * GET properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+  /**
+   * Update property details.
+   * PUT or PATCH properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+  }
+
+  /**
+   * Delete a property with id.
+   * DELETE properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await property.delete()
+  }
+}
+
+module.exports = PropertyController
